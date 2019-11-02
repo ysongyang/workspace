428db8b3b9aff311f189d7ef64393a9fdb9cea6c
zz85300371 <85300371@qq.com>
2019-11-02 17:25:40 +0800

    update

--- app/Controller/IndexController.php
+++ app/Controller/IndexController.php
@@ -15,60 +15,24 @@ use Hyperf\HttpServer\Annotation\Controller;
 use Hyperf\HttpServer\Annotation\GetMapping;
 
 /**
- * @Controller(prefix="v1")
+ * @Controller
  * Class IndexController
  * @package App\Controller
  */
 class IndexController extends AbstractController
 {
     /**
-     * @GetMapping(path="index",methods="get")
+     * @GetMapping(path="/",methods="get")
      * @return array
      */
     public function index()
     {
         $user = $this->request->input('user', 'Hyperf');
         $method = $this->request->getMethod();
-        $obj = \EasySwoole\Jwt\Jwt::getInstance()->algMethod('AES')->setSecretKey('测试呀')->publish();
-        $obj->setExp(time() + 3600);
-        $data['uid'] = 1008;
-        $data['username'] = 'ysongyang';
-        $obj->setData($data);
-        $token = $obj->__toString();
         return [
             'method' => $method,
             'message' => "Hello {$user}.",
-            'token' => $token
         ];
     }
 
-    public function create()
-    {
-        $jwt = \EasySwoole\Jwt\Jwt::getInstance();
-        // 实际可能是通过传参等方式获取token
-        $token = $this->request->input('token', null);
-
-        try {
-            /** @var \EasySwoole\Jwt\JwtObject $result */
-            $result = $jwt->decode($token);
-            $msg = '';
-            switch ($result->getStatus()) {
-                case -1:
-                    $msg = 'token无效';
-                case  1:
-                    $msg = '验证通过';
-                    break;
-                case  2:
-                    $msg = '验证失败';
-                    break;
-                case  3:
-                    $msg = 'token过期';
-                    break;
-            }
-            // 根据解密之后的结果完善业务逻辑
-            return ['result' => $result, 'status' => $result->getStatus(), 'msg' => $msg];
-        } catch (\Exception $e) {
-            // TODO: 处理异常
-        }
-    }
 }
